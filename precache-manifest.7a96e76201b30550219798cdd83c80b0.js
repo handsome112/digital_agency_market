de12f3af5fa82accc055d7cd1689e0fe9ce96ef7
handsome <handsome920112@gmail.com>
2023-01-03 23:09:31 -0800

    Updates

---
+++ precache-manifest.7a96e76201b30550219798cdd83c80b0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ccb0a0a1e14b74846ac9386b8b7df7",
+    "url": "/react-agency-website/index.html"
+  },
+  {
+    "revision": "e231a718fc58001c4f89",
+    "url": "/react-agency-website/static/css/main.6471b3ab.chunk.css"
+  },
+  {
+    "revision": "ca56d25624cf61288ad2",
+    "url": "/react-agency-website/static/js/2.0bc45ad3.chunk.js"
+  },
+  {
+    "revision": "e231a718fc58001c4f89",
+    "url": "/react-agency-website/static/js/main.fb106445.chunk.js"
+  },
+  {
+    "revision": "36e6d4d438a942a2597d",
+    "url": "/react-agency-website/static/js/runtime-main.327eb88e.js"
+  },
+  {
+    "revision": "10678e9b565649afcd53eeeb4968f87c",
+    "url": "/react-agency-website/static/media/logo.10678e9b.png"
+  },
+  {
+    "revision": "eb77bfb55efc0021e68a6186ce1cadc1",
+    "url": "/react-agency-website/static/media/p1.eb77bfb5.jpg"
+  },
+  {
+    "revision": "d13486a103ab741205326db427566332",
+    "url": "/react-agency-website/static/media/p2.d13486a1.jpg"
+  },
+  {
+    "revision": "2d13453bfed9b8622b5e2abd7399d7c9",
+    "url": "/react-agency-website/static/media/p3.2d13453b.jpg"
+  },
+  {
+    "revision": "b33b9181020763573debbb34c3920778",
+    "url": "/react-agency-website/static/media/p4.b33b9181.jpg"
+  },
+  {
+    "revision": "b4ea08bb3b40e600dbe457608289ccb3",
+    "url": "/react-agency-website/static/media/p5.b4ea08bb.jpg"
+  },
+  {
+    "revision": "57b8821639388c70be74a4d8a3a54ff7",
+    "url": "/react-agency-website/static/media/p6.57b88216.jpg"
+  },
+  {
+    "revision": "4164b7fa32d18a065900748edc6c4b09",
+    "url": "/react-agency-website/static/media/shape2.4164b7fa.png"
+  }
+]);
